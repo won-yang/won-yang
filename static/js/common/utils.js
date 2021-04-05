43d7623f52ac97087969f9219f0a38b6b41d15d6
mimseong <parkseongmin98@gmail.com>
2021-04-05 20:03:40 +0900

    [Add] add get tag

--- static/js/common/utils.js
+++ static/js/common/utils.js
@@ -3,3 +3,11 @@ function onlyNumber() {
     event.returnValue = false;
   }
 }
+
+const getTag = (post) => {
+  if (post.main_gate == '1') return '#창원대 정문';
+  else if (post.west_gate == '1') return '#기숙사 서문';
+  else if (post.east_gate == '1') return '#공대 동문';
+  else if (post.etc_gate == '1') return '#기타';
+  else return '#기타';
+};

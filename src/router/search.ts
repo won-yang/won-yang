f93607856da1645ef54c153edc7dd092794c0c6f
mimseong <parkseongmin98@gmail.com>
2021-05-14 15:31:17 +0900

    [Update] search.js

--- src/router/search.ts
+++ src/router/search.ts
@@ -3,6 +3,14 @@ const router = express.Router();
 import { sendQuery } from '../config/db';
 const permission = require('../function/permission_verify');
 
+const getTag = (post: any) => {
+  if (post.main_gate == '1') return '#창원대 정문';
+  else if (post.west_gate == '1') return '#기숙사 서문';
+  else if (post.east_gate == '1') return '#공대 동문';
+  else if (post.etc_gate == '1') return '#기타';
+  else return '#기타';
+};
+
 router.get('/search', async (req: any, res: any) => {
   const main_gate = check(req.query.main_gate);
   const east_gate = check(req.query.east_gate);
@@ -22,25 +30,19 @@ router.get('/search', async (req: any, res: any) => {
     return;
   }
 
+  const notice_rows = await sendQuery(`SELECT title, post_idx FROM notice ORDER BY post_date DESC`);
+
   const page = await getPageInfo(req);
+  const send_img = await getThumbNail(page.post_rows);
   const login_check = loginCheck(req);
-  let images = new Array();
-
-  for (const row of page.post_rows) {
-    const image = await sendQuery('SELECT image_path FROM thumbnail WHERE post_idx = ?', [row.post_idx]);
-    images.push(image);
-  }
-
-  const send_img = images.map((image) => {
-    if (image[0]) return image[0].image_path;
-    else return '/image/image_thumbnail.png';
-  });
 
   res.render('search', {
     post: page.post_rows,
+    notice: notice_rows,
     image: send_img,
     login_check: login_check,
     page_info: page.page_info,
+    getTag: getTag,
     main_gate: main_gate,
     east_gate: east_gate,
     west_gate: west_gate,
@@ -56,33 +58,74 @@ router.get('/my_post', async (req: any, res: any) => {
   }
 
   const user_id = req.session.passport.user.id;
+  const notice_rows = await sendQuery(`SELECT title, post_idx FROM notice ORDER BY post_date DESC`);
 
-  const search_rows = await sendQuery(
-    `SELECT post_idx, title, post_date, address, deposit, monthly_rent
-                                        FROM post NATURAl JOIN post_content
-                                        WHERE post_idx IN
-                                        	(SELECT post_idx FROM post NATURAL JOIN users WHERE user_id = ?)
-                                        ORDER BY post_date DESC`,
-    [user_id],
-  );
+  const page = await getPageInfoMyPost(req, user_id);
+  const send_img = await getThumbNail(page.post_rows);
+  const login_check = loginCheck(req);
 
-  let images = new Array();
-
-  for (const row of search_rows) {
-    const image = await sendQuery('SELECT image_path FROM thumbnail WHERE post_idx = ?', [row.post_idx]);
-    images.push(image);
-  }
-
-  const send_img = images.map((image) => {
-    if (image[0]) return image[0].image_path;
-    else return '/image/image_thumbnail.png';
-  });
-
-  res.json({ result: 'success', rows: search_rows, image: send_img });
+  res.json({ result: 'success', post: page.post_rows, notice: notice_rows, image: send_img, login_check: login_check, page_info: page.page_info});
 });
 
 const check = (val: string) => {
   return val == 'true' ? '1' : '-1';
+};
+
+const getPageInfoMyPost = async (req: any, userId: any) => {
+  const search = req.query.search != undefined ? `%${req.query.search}%` : '%%';
+  const main_gate = check(req.query.main_gate);
+  const east_gate = check(req.query.east_gate);
+  const west_gate = check(req.query.west_gate);
+  const etc_gate = check(req.query.etc_gate);
+  const monthly_rent = Number(req.query.monthly_rent) ? Number(req.query.monthly_rent) : 9999999999;
+
+  const pageNum = Number(req.query.pageNum) || 1;
+  const contentSize = 12;
+  const pnSize = 10; //페이지 사이즈
+  const skipSize = (pageNum - 1) * contentSize;
+
+  const totalCount = await sendQuery(
+    `SELECT count(*) as count
+                                            FROM post NATURAl JOIN post_content
+                                            WHERE post_idx IN
+                                            	(SELECT post_idx FROM post NATURAL JOIN tag WHERE main_gate = ? OR west_gate = ? OR east_gate = ? OR etc_gate = ?)
+                                            	AND
+                                            	title LIKE ?
+                                              AND
+                                              monthly_rent <= ?
+                                              AND
+                                              user_id = ?
+                                            ORDER BY post_date DESC;`,
+    [main_gate, west_gate, east_gate, etc_gate, search, monthly_rent, userId],
+  );
+  const pnTotal = Math.ceil(totalCount[0].count / contentSize); // NOTE: 페이지네이션의 전체 카운트
+  const pnStart = (Math.ceil(pageNum / pnSize) - 1) * pnSize + 1; // NOTE: 현재 페이지의 페이지네이션 시작 번호.
+  let pnEnd = pnStart + pnSize - 1;
+
+  const post_rows = await sendQuery(
+    `SELECT post_idx, title, post_date, address, deposit, monthly_rent
+                                    FROM post NATURAl JOIN post_content
+                                    WHERE post_idx IN
+                                    	(SELECT post_idx FROM post NATURAL JOIN tag WHERE main_gate = ? OR west_gate = ? OR east_gate = ? OR etc_gate = ?)
+                                    	AND
+                                    	title LIKE ?
+                                      AND
+                                      monthly_rent <= ?
+                                      AND
+                                      user_id = ?
+                                    ORDER BY post_date DESC LIMIT ${skipSize}, ${contentSize};`,
+    [main_gate, west_gate, east_gate, etc_gate, search, monthly_rent, userId],
+  );
+  if (pnEnd > pnTotal) pnEnd = pnTotal;
+
+  const page_info = {
+    pageNum,
+    pnStart,
+    pnEnd,
+    pnTotal,
+  };
+
+  return { page_info: page_info, post_rows: post_rows };
 };
 
 const getPageInfo = async (req: any) => {
@@ -136,6 +179,21 @@ const getPageInfo = async (req: any) => {
   };
 
   return { page_info: page_info, post_rows: post_rows };
+};
+
+const getThumbNail = async (post_rows: any) => {
+  const images = new Array();
+  for (const row of post_rows) {
+    const image = await sendQuery('SELECT image_path FROM thumbnail WHERE post_idx = ?', [row.post_idx]);
+    images.push(image);
+  }
+
+  const send_img = images.map((image) => {
+    if (image[0]) return image[0].image_path;
+    else return '/image/image_thumbnail.png';
+  });
+
+  return send_img;
 };
 
 const loginCheck = (req: any) => {

3f3a76cc993031dd875d80050f9f2025cc4e18d1
rockpell <rockpell.cleaning@gmail.com>
2021-07-01 22:44:28 +0900

    [chore] postgressql pg clint에서 pool로 변경

--- backend/src/route/app.ts
+++ backend/src/route/app.ts
@@ -1,21 +1,20 @@
 import * as express from 'express';
-import { Client, Query } from 'pg';
 
-const client = new Client({ user: 'postgres', host: 'localhost', database: 'postgres', password: 'mysecretpassword', port: 5432 });
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: 'wonyang',
+  host: 'localhost',
+  database: 'wonyang',
+  password: 'qwer',
+  port: 5432,
+});
 
 const app = express();
 
 import router from './hello';
 
 app.use(express.json());
-
-client.connect((err) => {
-  if (err) {
-    console.error('connection error', err.stack);
-  } else {
-    console.log('success!');
-  }
-});
 
 app.use('/api', router);
 

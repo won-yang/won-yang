c8238c6f129ec93415b8c803d593d0b33c20899f
mimseong <parkseongmin98@gmail.com>
2021-07-01 22:57:30 +0900

    [chore] 데이터베이스 테스트 코드 작성 - 미완

--- backend/src/route/app.ts
+++ backend/src/route/app.ts
@@ -18,6 +18,15 @@ app.use(express.json());
 
 app.use('/api', router);
 
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM films', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
 app.get('*', (req: any, res: any) => {
   // res.render('error/404');
   res.send('not found');
--- backend/src/route/hello.ts
+++ backend/src/route/hello.ts
@@ -1,8 +1,23 @@
 import * as express from 'express';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: 'wonyang',
+  host: 'localhost',
+  database: 'wonyang',
+  password: 'qwer',
+  port: 5432,
+});
+
 const router = express.Router();
 
-const hello = (req: any, res: any) => {
-  return res.json({ text: 'hello world' });
+const hello = (request, response) => {
+  pool.query('SELECT * FROM films', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
 };
 
 router.get('/', hello);

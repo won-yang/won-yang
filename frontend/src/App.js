757ebdcdcd8553642fcbb1b4400e3ccf18c19c67
Seongsangcho <liasqui31@gmail.com>
2021-07-01 22:26:15 +0900

    [chore] props-type install

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import UnivSearchbar from 'components/UnivSearchbar';
+import Header from 'components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
+      <Header></Header>
       <UnivSearchbar></UnivSearchbar>
     </div>
   );

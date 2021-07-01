06e1ae160a32a170dc3e8ad944e0d10af6e83674
Seongsangcho <liasqui31@gmail.com>
2021-07-01 21:57:36 +0900

    [chore] 초기환경세팅

---
+++ frontend/src/components/UnivSearchbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UnivSearchbar = (props) => {
+  return (
+    <div>
+      <input></input>
+    </div>
+  );
+};
+
+UnivSearchbar.propTypes = {};
+
+export default UnivSearchbar;

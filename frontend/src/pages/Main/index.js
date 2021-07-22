e11f4c00ae20485825948578436705b05cb06ca2
Seongsangcho <liasqui31@gmail.com>
2021-07-22 22:56:49 +0900

    [style]#8 container padding
    - header가 fixed여서 padding top 추가

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from 'components/Header';
+import { ArticleContainer } from './style';
 
 const MainPage = (props) => {
-  return <Header></Header>;
+  return (
+    <>
+      <Header></Header>
+      <ArticleContainer>
+        <section>검색 및 공지사항 div</section>
+        <section>필터링 버튼</section>
+        <section>게시글 리스트</section>
+      </ArticleContainer>
+    </>
+  );
 };
 
 MainPage.propTypes = {};
---
+++ frontend/src/pages/Main/style.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const ArticleContainer = styled.article`
+  padding-top: 60px;
+`;

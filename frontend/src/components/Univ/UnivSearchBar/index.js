2e1452f4544dd9f32366e2a2361038a0d9503907
blingblin-g <luvbliny@gmail.com>
2021-08-19 00:10:42 +0900

    feat request api

--- frontend/src/components/Univ/UnivSearchBar/index.js
+++ frontend/src/components/Univ/UnivSearchBar/index.js
@@ -1,21 +1,42 @@
 import React, { useState } from 'react';
 import { debounce } from 'lodash';
 
+const BASE_URL = 'http://localhost:8080';
+
 const UnivSearchbar = (props) => {
   const [inputValue, setInputValue] = useState('');
 
-  const onChangeHandling = (e) => {
+  const onChangeHandler = (e) => {
     setInputValue(e.target.value);
     console.log(inputValue);
   };
 
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    const request = async () => {
+      try {
+        const UNIV_API = '/api/school';
+
+        const response = await fetch(
+          `${BASE_URL}${UNIV_API}?name=${inputValue}`
+        );
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    request();
+  };
+
   return (
-    <form name='univ-search'>
+    <form name='univ-search' onSubmit={onSubmitHandler}>
       <input
         name='name'
         type='search'
         value={inputValue}
-        onChange={onChangeHandling}
+        onChange={onChangeHandler}
         placeholder='대학교를 입력해주세요'
       />
     </form>

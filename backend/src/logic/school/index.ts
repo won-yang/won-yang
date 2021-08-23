fd47997cdc487a8f9e64e231e67a973f02dcb85f
mimseong <parkseongmin98@gmail.com>
2021-08-23 21:01:21 +0900

    [refactor] 안 쓰는 school file 삭제

--- backend/src/logic/school/index.ts
+++
@@ -1,7 +0,0 @@
-import { IUniversity } from '../../interface/interface';
-import * as school_db from '../../db/school';
-
-export const getSchoolList = async (name: string): Promise<IUniversity[]> => {
-  const school = await school_db.getSchoolByName(name);
-  return school;
-};

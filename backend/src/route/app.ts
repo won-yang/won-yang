55e21ef87cbb4b64e0eb644f094b401c3246d527
rockpell <aythffk@gmail.com>
2021-08-28 23:23:23 +0900

    [feat] 에러 처리를 위한 미들웨어 추가
    - router next의 인자에 값을 넣는 경우 에러로 인식하여 이 함수 호출

--- backend/src/route/app.ts
+++ backend/src/route/app.ts
@@ -26,6 +26,11 @@ app.get('*', (req: any, res: any) => {
   res.send('not found');
 });
 
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('server error!');
+});
+
 app.listen(8080, () => {
   console.log('Running...');
 });

82ee5de112b857e5e7fe7aca8b6214798f32e4f7
genie <luvbliny@gmail.com>
2021-09-08 23:27:41 +0900

    style : layout of signup pages

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,40 +1,53 @@
 import React from "react";
 import { IconLogo } from "components/Icon";
 import { ReactComponent as IconSearch } from "assets/icon_search.svg";
-import { SignUpHeader, SignUpContainer, SignUpForm } from "./style";
+import {
+  SignUpHeader,
+  SignUpContainer,
+  SignUpForm,
+  FormHeader,
+  LabelContainer,
+  InputContainer,
+} from "./style";
 
 const SignUpPage = () => {
   return (
     <SignUpContainer>
       <SignUpHeader>
-        <IconLogo widthSize='100px' heightSize='100px' />
+        <IconLogo widthSize='12em' heightSize='12em' />
       </SignUpHeader>
       <SignUpForm>
-        <h2 className='header'>정보를 알려주세요</h2>
-        <label className='univ univ__label' htmlFor='university'>
-          소속 대학교
-          <span className='univ info univ__info'>
+        <FormHeader>정보를 알려주세요</FormHeader>
+        <LabelContainer htmlFor='university'>
+          <h3 className='header'>소속 대학교</h3>
+          <div className='univ info univ__info'>
             검색 버튼을 눌러 대학교를 입력해주세요
-          </span>
-        </label>
-        <div>
+          </div>
+        </LabelContainer>
+        <InputContainer>
           <input
-            className='univ signup__form'
+            className='univ__search signup__form'
             type='text'
             name='university'
           ></input>
-          <button className='search__univ'>icon</button>
-        </div>
+          <button className='search__univ'>
+            <IconSearch width='35px' height='35px' />
+          </button>
+        </InputContainer>
 
-        <label className='univ nickname__label' htmlFor='nickname'>
-          닉네임
-          <span className='univ info nickname__info'>
+        <LabelContainer htmlFor='nickname'>
+          <h3 className='header'>닉네임</h3>
+          <div className='univ info nickname__info'>
             2~8자 한글, 영어로만 작성해주세요.
-          </span>
-        </label>
-        <div>
-          <input className='signup__form' type='text' name='nickname'></input>
-        </div>
+          </div>
+        </LabelContainer>
+        <InputContainer>
+          <input
+            className='nickname signup__form'
+            type='text'
+            name='nickname'
+          ></input>
+        </InputContainer>
         <button className='signup__conplete'>작성 완료</button>
       </SignUpForm>
     </SignUpContainer>
--- frontend/src/pages/SignUp/style.js
+++ frontend/src/pages/SignUp/style.js
@@ -18,6 +18,7 @@ export const SignUpContainer = styled.div`
 export const SignUpHeader = styled.header`
   background-color: #4593ef;
   width: 100vw;
+  height: 12em;
 
   div {
     margin: auto;
@@ -25,7 +26,6 @@ export const SignUpHeader = styled.header`
 `;
 
 export const SignUpForm = styled.form`
-  /*border: 1px solid black;*/
   display: flex;
   flex-direction: column;
 
@@ -39,8 +39,53 @@ export const SignUpForm = styled.form`
     color: #515151;
   }
 
+  .signup__form {
+    width: 20rem;
+    height: 2.5em;
+  }
+
   .signup__conplete {
     background-color: #4593ef;
     color: white;
+    margin: auto;
+    margin-top: 1em;
+    width: 10rem;
+    height: 3em;
+    font-size: 1.2rem;
+    border-radius: 10px;
+  }
+`;
+
+export const FormHeader = styled.h2`
+  font-size: 1.8em;
+  text-align: center;
+  margin: 20px 0 35px 0;
+`;
+
+export const LabelContainer = styled.label`
+  font-size: 1.4em;
+  display: flex;
+  .header {
+    margin-right: 6px;
+  }
+
+  .info {
+    line-height: 20px;
+  }
+`;
+
+export const InputContainer = styled.div`
+  margin-bottom: 25px;
+
+  .univ__search {
+    transform: translateY(-11px);
+  }
+
+  .search__univ {
+    background-color: #2e42a9;
+  }
+
+  .nickname {
+    width: 23rem;
   }
 `;

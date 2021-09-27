e58dd4cb2999aaa2d1c2d967e7b797706b36d03e
genie <luvbliny@gmail.com>
2021-09-27 21:21:39 +0900

    refactor sigle quotes to double quotes

--- frontend/src/components/Icon/IconLogo.js
+++ frontend/src/components/Icon/IconLogo.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { ReactComponent as Logo } from 'assets/logo.svg';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import IconContainer from './IconContainer';
+import React from "react";
+import PropTypes from "prop-types";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import IconContainer from "./IconContainer";
 
 const IconLogo = (props) => {
   return (

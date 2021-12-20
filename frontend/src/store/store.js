d3bf6212f85e934b26af142515ed939ad88f8c5b
seongsangcho <liasqui31@gmail.com>
2021-12-20 22:33:27 +0900

    chore: user slice

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,12 @@
 import { configureStore, getDefaultMiddleware, combineReducers } from "@reduxjs/toolkit";
 import postwriteReducer from "./Postwrite/PostwriteSlice";
 import universityReducer from "./University/UniversitySlice";
+import userReducer from "./User/userSlice";
 
 // const reducer = combineReducers({ postwriteReducer, universityReducer });
 
 export const store = configureStore({
-  reducer: combineReducers({ postwriteReducer, universityReducer }),
+  reducer: combineReducers({ postwriteReducer, universityReducer, userReducer }),
   middleware: getDefaultMiddleware({
     serializableCheck: false,
   }),
